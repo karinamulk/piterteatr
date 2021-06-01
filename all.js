3d28c6d73e69b4d4cc986ddead49bb474407684a
karinamulk <70967051+karinamulk@users.noreply.github.com>
2021-06-01 22:32:05 +0300

    Update all.js

--- all.js
+++ all.js
@@ -1456,33 +1456,3 @@ $(document).ready(function(){
 
 
 });
-
-
-let topMenu = $("#menu"),
-    topMenuHeight = topMenu.outerHeight()+15,
-    // All list items
-    menuItems = topMenu.find("a"),
-    // Anchors corresponding to menu items
-    scrollItems = menuItems.map(function(){
-      let item = $($(this).attr("href"));
-      if (item.length) { return item; }
-    });
-
-// Bind to scroll
-// $(window).scroll(function(){
-//    // Get container scroll position
-//    let fromTop = $(this).scrollTop()+topMenuHeight;
-//
-//    // Get id of current scroll item
-//    let cur = scrollItems.map(function(){
-//      if ($(this).offset().top < fromTop)
-//        return this;
-//    });
-//    // Get the id of the current element
-//    cur = cur[cur.length-1];
-//    let id = cur && cur.length ? cur[0].id : "";
-//    // Set/remove active class
-//    menuItems
-//      .parent().removeClass("active")
-//      .end().filter("[href='#"+id+"']").parent().addClass("active");
-// });​
